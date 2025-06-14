74738222e83128c7080f317e54e6a5ba17f16980
Leah <shlemov@tuta.io>
2025-06-14 15:07:12 +0300

    finished i2c testing

--- Core/Src/stm32f7xx_it.c
+++ Core/Src/stm32f7xx_it.c
@@ -95,7 +95,6 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-	printf("Hard Fault\n");
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
