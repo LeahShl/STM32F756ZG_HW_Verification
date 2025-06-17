77a0fc8e7b3602cbb63f037a216e0c64a569146c
Leah <shlemov@tuta.io>
2025-06-17 22:13:38 +0300

    updated usage examples to reflect recent changes

--- FILES_FOR_PC/usage_example.sh
+++ FILES_FOR_PC/usage_example.sh
@@ -4,11 +4,28 @@ EXECUTABLE="./hw_tester"
 $EXECUTABLE --all 
 
 # Below are usage examples.
-# Uncomment the following lines to run them
+# Uncomment  any of the following lines to run them
 
-# $EXECUTABLE -usi "Hello, world!"
-# $EXECUTABLE -u "hello1" -s "hello2" -i "hello3"
-# $EXECUTABLE -at
-# $EXECUTABLE -u "Hello, world!" -si "FooBar" -t
-# $EXECUTABLE -uat
+# Individual peripheral tests
 
+# $EXECUTABLE -u "UART test message"    # UART test (Optional custom msg)
+# $EXECUTABLE -s "SPI test message"     # SPI test  (Optional custom msg)
+# $EXECUTABLE -i "I2C test message"     # I2C test  (Optional custom msg)
+# $EXECUTABLE -a                        # ADC test
+# $EXECUTABLE -t                        # Timer test
+
+# Combined tests
+
+# $EXECUTABLE -usi "Shared message"     # Multiple peripherals
+# $EXECUTABLE --all "Universal test"    # All peripherals
+
+# Set number of iterations
+
+# $EXECUTABLE -u -n 5                   # 5 iterations
+# $EXECUTABLE --all -n 20               # 20 iterations each
+
+# Data retrieval
+
+# $EXECUTABLE get 1 5 10               # Print specific test results
+# $EXECUTABLE export                   # Print all data in a CSV format
+# $EXECUTABLE export > "out.csv"       # You can redirect data to any file
